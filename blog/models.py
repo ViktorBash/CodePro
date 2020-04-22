24906aa6e696b6ec10f2ab7c61b0168f7091413b
Viktor Basharkevich <basharkevichv@gmail.com>
2020-04-22 00:23:22 -0400

    Fixed bug and wrote a test
    Fixed a bug with creating new posts.
    Wrote the first testcase, and will be adding onto it.

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.IntegerField()
+    likes = models.IntegerField(default=1)
 
     def __str__(self):
         return self.title
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
-
+from blog.models import Post
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        User1 = User.objects.create_user(username="Jacob", email="jacob@google.com", password="Password1234")
+        User2 = User.objects.create_user(username="Robby", email="baseballover@gmail.com", password="Kirk09")
+        Post.objects.create(title="My First Post", content="Wow! I love learning Python so much that I started a blog because of it!", author=User1)
+        Post.objects.create(title="Experienced Blogger", content="This is my second post, and now I am an experienced blogger.", author=User2)
+
+    def test_posts(self):
+        # Testing out our Post models
+        post1 = Post.objects.get(title="My First Post")
+        post2 = Post.objects.get(title="Experienced Blogger")
+        User1 = User.objects.get(username="Jacob")
+        User2 = User.objects.get(username="Robby")
+        self.assertEqual(User1.email, "jacob@google.com")
+        self.assertEqual(User2.username, "Robby")
+        self.assertEqual(post1.author, User1)
+        print("Test")
